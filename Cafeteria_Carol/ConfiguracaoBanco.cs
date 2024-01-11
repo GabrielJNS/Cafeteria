45e270fe3c4a5bfa91f8989a514c49ece2da76bf
GabrielJNS <gabriel.silva678@fatec.sp.gov.br>
2024-01-11 15:42:47 -0300

    ProjetoCefeteria

--- Cafeteria_Carol/ConfiguracaoBanco.cs
+++ Cafeteria_Carol/ConfiguracaoBanco.cs
@@ -9,7 +9,7 @@ namespace Cafeteria_Carol
 
     public static class ConfiguracaoBanco
     {
-        public static string CaminhoBanco { get; } = "Data Source=C://Users//gabri//Downloads//Projeto_Cafeteria//Cafeteria_Carol//Banco//bd_cafeteria.db";
+        public static string CaminhoBanco { get; } = "Data Source=C:/Users/gabri/source/repos/ProjetoCafeteriaFinal/Cafeteria_Carol/Banco/bd_cafeteria.db";
     }
 }
 
